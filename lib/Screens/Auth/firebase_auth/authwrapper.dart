6a4a3e3f7fbccb45c7f71440d67feb8a63441cdc
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-05-20 22:56:04 +0530

    feat: AuthWrapper added

--- lib/Screens/Auth/firebase_auth/authwrapper.dart
+++ lib/Screens/Auth/firebase_auth/authwrapper.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:insighteye_app/Screens/Admin/admin_home_src.dart';
 import 'package:insighteye_app/Screens/Auth/login_src.dart';
+import 'package:insighteye_app/Screens/homeWrapper.dart';
 import 'package:provider/provider.dart';
 
-
 class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({super.key});
   @override
   Widget build(BuildContext context) {
     final firebaseUser = context.watch<User?>();
 
     if (firebaseUser != null) {
-      return const AdminHomeSrc();
+      return const HomeWrapper();
     }
     return const LoginSrc();
   }
